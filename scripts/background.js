4f08a6db76ff8a75152fab5c9d2c74b7a33fec0b
william klingler <klinglew@gmail.com>
2022-01-08 14:44:04 +0000

    auto push

--- scripts/background.js
+++ scripts/background.js
@@ -4,3 +4,10 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+	// for the current tab, inject the "inject.js" file & execute it
+	chrome.tabs.executeScript(tab.ib, {
+		file: 'inject.js'
+	});
+});
